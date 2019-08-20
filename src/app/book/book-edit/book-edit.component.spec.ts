bfdf0143550b03df0d172055d5194518ffc14ad7
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-08-20 16:37:55 +0200

    test: check if form has been submitted

--- src/app/book/book-edit/book-edit.component.spec.ts
+++ src/app/book/book-edit/book-edit.component.spec.ts
@@ -1,52 +1,79 @@
-import { RouterTestingModule } from '@angular/router/testing';
-import { BookStaticAsyncDataService } from './../shared/book-static-async-data.service';
-import { BookDataService } from '../shared/book-data.service';
-import { ComponentFixture, TestBed, async, fakeAsync, tick } from '@angular/core/testing';
-
-import { BookEditComponent } from './book-edit.component';
-import { DebugElement } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
-
+import { By } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Book } from '../shared/book';
+import { BookDataService } from '../shared/book-data.service';
+import { BookStaticAsyncDataService } from './../shared/book-static-async-data.service';
+import { BookEditComponent } from './book-edit.component';
 
 describe('BookEditComponent', () => {
   let component: BookEditComponent;
   let fixture: ComponentFixture<BookEditComponent>;
   let compiled;
+  let service: BookDataService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        BookEditComponent
-      ],
-      imports: [
-        FormsModule,
-        RouterTestingModule
-      ],
-      providers: [{ provide: BookDataService, useClass: BookStaticAsyncDataService }]
-    })
-      .compileComponents();
+      declarations: [BookEditComponent],
+      imports: [FormsModule, RouterTestingModule],
+      providers: [
+        { provide: BookDataService, useClass: BookStaticAsyncDataService }
+      ]
+    }).compileComponents();
+
+    service = TestBed.get(BookDataService);
   });
 
+  // async have to stay since we use template driven forms
   beforeEach(async(() => {
     fixture = TestBed.createComponent(BookEditComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
     compiled = fixture.debugElement.nativeElement;
-  }));
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
-  });
+    fixture.detectChanges();
+  }));
 
   it('should display the isbn of the loaded book', () => {
-    expect(true).toBeFalsy();
+    const isbn = '978-0-20163-361-0';
+    component.book = { isbn } as Book;
+
+    const isbnInput: HTMLInputElement = fixture.debugElement.query(
+      By.css('[data-testid=isbn-input]')
+    ).nativeElement;
+
+    expect(isbnInput.value).toBe(isbn);
   });
 
-  it('should validate that title is required and show an error message', fakeAsync(() => {
-    expect(true).toBeFalsy();
-  }));
+  it('should validate that title is required and show an error message', () => {
+    const isbn = '978-0-20163-361-0';
+    component.book = { isbn } as Book;
+
+    const titleInput: HTMLInputElement = fixture.debugElement.query(
+      By.css('[data-testid=title-input]')
+    ).nativeElement;
+    titleInput.value = '';
+    titleInput.dispatchEvent(new Event('input'));
+
+    fixture.detectChanges();
+
+    const validationMessage: HTMLElement = fixture.debugElement.query(
+      By.css('[data-testid=title-validation-message]')
+    ).nativeElement;
+
+    expect(validationMessage.hasAttribute('hidden')).toBe(false);
+  });
 
   it('should submit the whole form on click on the submit button', () => {
-    expect(true).toBeFalsy();
+    const updateBookSpy = spyOn(service, 'updateBook');
+
+    const submitButton: HTMLButtonElement = fixture.debugElement.query(
+      By.css('[data-testid=book-submit]')
+    ).nativeElement;
+
+    submitButton.click();
+    fixture.detectChanges();
+
+    expect(updateBookSpy).toHaveBeenCalled();
   });
 });
--- src/app/book/book-edit/book-edit.component.ts
+++ src/app/book/book-edit/book-edit.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Book } from '../shared/book';
 import { ActivatedRoute } from '@angular/router';
-import { BookDataService } from '../shared/book-data.service';
 import { mergeMap } from 'rxjs/operators';
+import { Book } from '../shared/book';
+import { BookDataService } from '../shared/book-data.service';
 
 @Component({
   selector: 'book-edit',
@@ -10,22 +10,26 @@ import { mergeMap } from 'rxjs/operators';
   styleUrls: ['./book-edit.component.css']
 })
 export class BookEditComponent implements OnInit {
-
   book: Book;
 
-  constructor(private route: ActivatedRoute, private bookService: BookDataService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private bookService: BookDataService
+  ) {}
 
   ngOnInit() {
-    this.route.params.pipe(
-      mergeMap((params: { isbn: string }) => this.bookService.getBookByIsbn(params.isbn))
-    )
-      .subscribe(book => this.book = book);
+    this.route.params
+      .pipe(
+        mergeMap((params: { isbn: string }) =>
+          this.bookService.getBookByIsbn(params.isbn)
+        )
+      )
+      .subscribe(book => (this.book = book));
   }
 
   onSubmit(value) {
-
-    this.bookService.updateBook(this.book.isbn, value)
+    this.bookService
+      .updateBook(this.book.isbn, value)
       .subscribe((book: Book) => console.log('Book updated', book));
   }
-
 }
