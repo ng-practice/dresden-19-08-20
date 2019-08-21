cccee1a9fcf427006169f028b899b80101c37c49
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-08-21 13:25:08 +0200

    chore: install effects and store-devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { metaReducers, reducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
+import { AppEffects } from './app.effects';
 
 @NgModule({
   declarations: [AppComponent, NavigationComponent],
@@ -23,7 +27,9 @@ import { metaReducers, reducers } from './reducers';
         strictActionSerializability: true,
         strictStateSerializability: true
       }
-    })
+    }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    EffectsModule.forRoot([AppEffects])
   ],
   providers: [],
   bootstrap: [AppComponent]
