06412af3c12beeabab7afc5e16d76703a37584d0
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-08-22 11:20:38 +0200

    chore: install ngrx/entity

--- src/app/book/book-edit/book-edit.component.ts
+++ src/app/book/book-edit/book-edit.component.ts
@@ -2,7 +2,6 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 import { Subscription } from 'rxjs';
 import { Book } from '../shared/book';
-import { BookDataService } from '../shared/book-data.service';
 import { State } from '../store';
 import { updateBook } from '../store/book.actions';
 import { currentBook } from '../store/book.selectors';
@@ -16,10 +15,7 @@ export class BookEditComponent implements OnInit, OnDestroy {
   book: Book;
   sink = new Subscription();
 
-  constructor(
-    private store: Store<State>,
-    private bookService: BookDataService
-  ) {}
+  constructor(private store: Store<State>) {}
 
   ngOnInit(): void {
     this.sink.add(
